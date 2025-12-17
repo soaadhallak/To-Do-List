17eeb6551ceed954971deae215f71b5dd3aaf7f6
soaadhallak <soaadhallak2@gmail.com>
2025-12-17 09:53:47 +0300

    update testcontroller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,10 +6,5 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-   public $aws_secret_key = "AKIAIMNO7YBX3TOFB98Q";
-
-   public function test(Request $request) {
-    // ثغرة XSS: طباعة مدخلات المستخدم مباشرة دون فلترة
-    echo "User Input: " . $request->query('name'); 
-}
+   
 }
